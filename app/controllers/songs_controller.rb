56a8ef130cf4fa9a7200c6d63ad5c18eff8c5782
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-10-15 15:53:46 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -46,7 +46,7 @@ class SongsController < ApplicationController
     if params[:artist_id]
       artist = Artist.find_by(id: params[:artist_id])
       if artist.nil?
-    x    redirect_to artsit_path, alert: "Artist not found"
+        redirect_to artsit_path, alert: "Artist not found"
       else
         @song = artist.songs.find_by(id: params[:id])
         redirect_to artist_songs_path(artist), alert: "Song not found." if song.nil?
@@ -78,7 +78,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_name, :artist_id)
   end
 end
 
